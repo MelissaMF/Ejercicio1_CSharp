13d98656719f1387f9ddf4a980d1123f5d6741b3
Melissa Muñoz Flández <melissa.munoz1@virginiogomez.cl>
2023-03-22 09:22:00 -0300

    Agregar archivos de proyecto.

---
+++ Clase_21_03_23/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public virtual void mostrar()
+        {
+            Console.WriteLine("Esta persona se llama {0} y su edad es {1}", Nombre , Edad);
+        }
+
+    }
+    public class Empleado : Persona
+    {
+        private int sueldo_bruto;
+
+        public int SueldoBruto
+        {
+            get { return sueldo_bruto; }
+            set { sueldo_bruto = value; }
+        }
+
+        public override void mostrar()
+        {
+            Console.WriteLine("El empleado se llama {0}, tiene {1}, años y su sueldo bruto es de {2}", Nombre, Edad, SueldoBruto);
+        }
+        private void calcular_salario_neto()
+        {
+            Console.WriteLine(sueldo_bruto * 0.83);
+        }
+
+    }
+
+    public class Cliente : Persona
+    {
+        private int telefono_de_contacto;
+
+        public int TelefonoDeContacto
+        {
+            get { return telefono_de_contacto; }
+            set { telefono_de_contacto = value; }
+        }
+
+        public override void mostrar()
+        {
+            Console.WriteLine("El cliente se llama {0}, tiene {1} años y su número de teléfono es {2}", Nombre, Edad, TelefonoDeContacto);
+        }
+    }
+
+
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona();
+        persona.Nombre= "Miyuki";
+        persona.Edad =26;
+        persona.mostrar();
+
+        Empleado empleado = new Empleado();
+        empleado.Nombre = "Hiroshi";
+        empleado.Edad = 25;
+        empleado.SueldoBruto = 150000;
+        empleado.mostrar();
+
+
+        Cliente cliente = new Cliente();
+        cliente.Nombre = "Mara";
+        cliente.Edad = 31;
+        cliente.TelefonoDeContacto = 988776655;
+        cliente.mostrar();
+    }
+}
+
+
